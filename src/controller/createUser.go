1694b2693c86e3fcdf52bf4d657e81a01df601e5
Lais Costa de Oliveira <lais.oliveira@gruporecursos.intranet>
2023-11-28 13:50:24 -0300

    criando objetos do usuário

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/laisolvx/crud-golang/src/configuration/rest_err"
+	"github.com/laisolvx/crud-golang/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadResquetError("Você chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadResquetError(
+			fmt.Sprintf("The are some incorrect filds, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
