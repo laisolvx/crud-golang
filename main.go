d6ff77c30ddfa36efb368c304cf13f2bec03f5fc
Lais Costa de Oliveira <lais.oliveira@gruporecursos.intranet>
2023-11-28 09:50:30 -0300

    padronizando erros e objetos

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import (
+import ( // importa pacotes necessários: fmt para formatação de saída, log para registro de erros, os para interação com o sistema operacional e github.com/joho/godotenv para carregar variáveis de ambiente de um arquivo chamado .env.
 	"fmt"
 	"log"
 	"os"
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
+	err := godotenv.Load() // tenta carregar as variáveis de ambiente do arquivo .env usando a função godotenv.Load().
 	if err != nil {
-		log.Fatal("error loading .env file")
+		log.Fatal("error loading .env file") // se houver algum erro ao carregar o arquivo .env, o programa exibe uma mensagem de erro usando log.Fatal() e encerra.
 	}
-	fmt.Println(os.Getenv("TEST"))
+	fmt.Println(os.Getenv("TEST")) // caso contrário, imprime na tela o valor associado à variável de ambiente "TEST" usando os.Getenv("TEST").
 }
